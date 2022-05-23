ae615ee16ef39b13471f4a6711a54158a2ebd161
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-05-23 17:48:00 -0400

    added JS File

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,29 @@
 function deafGrandma() {
-
+      let count=0
+      let text=window.prompt("What would you like to say to grandma?")
+      let lowerRegex=/[a-z]/
+      let upperRegex=/[A-Z]/
+      if (text=="") {
+            text=window.prompt("WHAT?!")
+            deafGrandma(text)
+      }
+      else if (text.match(lowerRegex)) {
+            text=window.prompt("SPEAK UP, KID!") 
+            deafGrandma(text)     
+      }
+      else if (text.match(upperRegex)) {
+            text=window.prompt("NO, NOT SINCE 1946!") 
+            deafGrandma(text)     
+      }
+      else if (text==="GOODBYE!" && count===0) {
+            text=window.prompt("LEAVING SO SOON?")
+            count+=1
+            deafGrandma(text)
+      }
+      
+      else if (text==="GOODBYE!" && count!==0) {
+            return alert("LATER, SKATER!")
+      }
 }
 
 deafGrandma();
