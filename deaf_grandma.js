4d505a72f2fbc12fdce336bfdaac320352d2490e
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2022-05-25 15:12:18 -0400

    Update deaf_grandma.js

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,3 +1,32 @@
+// function deafGrandma() {
+//       let count=0
+//       let text=window.prompt("HEY KID!")
+//       // let lowerRegex=/[a-z]/
+//       // let upperRegex=/[A-Z]/
+//       if (text=="") {
+//             text=window.prompt("WHAT?!")
+//             deafGrandma()
+//       }
+//       else if (text!== "" && text!== text.toLowerCase()) {
+//             text=window.prompt("SPEAK UP, KID!") 
+//             deafGrandma()     
+//       }
+//       else if (text!== "" && text===text.toUpperCase()) {
+//             text=window.prompt("NO, NOT SINCE 1946!") 
+//             deafGrandma()     
+//       }
+//       else if (text==="GOODBYE!" && count===0) {
+//             text=window.prompt("LEAVING SO SOON?")
+//             count+=1
+//             deafGrandma()
+//       }
+      
+//       else if (text==="GOODBYE!" && count!==0) {
+//             return alert("LATER, SKATER!")
+//       }
+// }
+
+// deafGrandma();
 
 function deafGrandma() {
     counter=0
